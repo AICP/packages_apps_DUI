8ea4d5821558b83d7d9c4cc90b8733015f77629e
ezio84 <brabus84@gmail.com>
2017-04-03 11:10:01 +0200

    Improve Pulse and MediaMonitor reliability
    fixes these situations:
    - pulse render sometimes not showing when starting to play streaming music
    with Google Music (we needed to give more time to the Visualizer to check
    valid bytes)
    - fix pulse not showing till a new tap on play/pause button
    in some players when u were listening to music, then paused it, then
    pressed the next/previous track button (the player didn't start playing yet
    e.g. Pulsar), then pressed play again.
    - this possibily fixes some other weird pulse/arrows behaviors around
    
    Change-Id: I26f4148ecec1f378697bce4867458f9bbdf4a568

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -240,6 +240,7 @@ public class SmartBarView extends BaseNavigationBar {
         mMusicStreamMuted = isMusicMuted(AudioManager.STREAM_MUSIC);
         IntentFilter filter = new IntentFilter();
         filter.addAction(AudioManager.STREAM_MUTE_CHANGED_ACTION);
+        filter.addAction(AudioManager.VOLUME_CHANGED_ACTION);
         context.registerReceiver(mReceiver, filter);
 
         mNavDoubleTapToSleep = new GestureDetector(context,
@@ -257,6 +258,10 @@ public class SmartBarView extends BaseNavigationBar {
                 if (mImeHintMode == 3) {
                     setNavigationIconHints(mNavigationIconHints, true);
                 }
+            }
+            @Override
+            public void areMetadataChanged() {
+                setNavigationIconHints(mNavigationIconHints, true);
             }
         };
         mMediaMonitor.setListening(true);
